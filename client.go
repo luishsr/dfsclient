9d233f34b01ae3ca13dd798c5b6225ae5cacccea
luishsr <luishsoares@gmail.com>
2023-04-26 22:28:56 -0300

    Updated comments

--- client.go
+++ client.go
@@ -1,3 +1,5 @@
+// Client is a sample implementation to test the
+// distributed file system
 package main
 
 import (
@@ -8,19 +10,23 @@ import (
 )
 
 func main() {
+
+	// List running Nodes
 	nodes := []filesys.Node{
 		{"localhost", 8000}, {"localhost", 8001},
 	}
 
+	// Spins up the distributed file system's component
 	fs := filesys.NewSimpleDistributedFileSystem(nodes)
 
-	// Upload a file
+	// Checks the local file before submitting
 	testFile, err := os.Open("test.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer testFile.Close()
 
+	// Uploads a file
 	err = fs.Put(testFile)
 	if err != nil {
 		fmt.Println("Failed to upload file:", err)
@@ -28,7 +34,7 @@ func main() {
 	}
 	fmt.Println("File uploaded successfully")
 
-	// Download the file
+	// Downloads the file
 	downloadedFile, err := fs.Get("test.txt")
 	if err != nil {
 		fmt.Println("Failed to download file:", err)
