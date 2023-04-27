913e004ebe7f667f16d18fc7b3d64702637200ce
luishsr <luishsoares@gmail.com>
2023-04-26 21:43:27 -0300

    Added dependencies

---
+++ client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	nodes := []Node{
+		{"localhost", 8000},
+	}
+
+	fs := NewSimpleDistributedFileSystem(nodes)
+
+	// Upload a file
+	testFile, err := os.Open("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer testFile.Close()
+
+	err = fs.Put(testFile)
+	if err != nil {
+		fmt.Println("Failed to upload file:", err)
+		return
+	}
+	fmt.Println("File uploaded successfully")
+
+	// Download the file
+	downloadedFile, err := fs.Get("test.txt")
+	if err != nil {
+		fmt.Println("Failed to download file:", err)
+		return
+	}
+	defer downloadedFile.Close()
+	defer os.Remove(downloadedFile.Name())
+
+	fmt.Println("File downloaded successfully:", downloadedFile.Name())
+}
